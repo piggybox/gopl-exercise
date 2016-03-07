5959ec0522c662e1b0e841dd073f3ff62337f2d0
Sky Yin <sky.yin@gmail.com>
2016-03-06 23:50:44 -0800

    Ex 1.12 paramtered lissajous service

--- ch1/server3/main.go
+++ ch1/server3/main.go
@@ -7,31 +7,62 @@
 package main
 
 import (
-	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
 	"log"
+	"math"
+	"math/rand"
 	"net/http"
+	"strconv"
 )
 
 func main() {
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		cycles, err := strconv.Atoi(r.FormValue("cycles"))
+		if err != nil {
+			http.Error(w, "Wrong input value for cycles", 500)
+			return
+		}
+
+		lissajous(w, cycles)
+	}
+
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
-//!+handler
-// handler echoes the HTTP request.
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
-	for k, v := range r.Header {
-		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-	}
-	fmt.Fprintf(w, "Host = %q\n", r.Host)
-	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
-	if err := r.ParseForm(); err != nil {
-		log.Print(err)
-	}
-	for k, v := range r.Form {
-		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
-	}
-}
+var palette = []color.Color{color.RGBA{34, 139, 34, 0xff}, color.Black, color.White}
 
-//!-handler
+const (
+	greenIndex = 0 // first color in palette
+	blackIndex = 1 // next color in palette
+	whiteIndex = 2
+)
+
+func lissajous(out io.Writer, cycles int) {
+	const (
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < float64(cycles)*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				whiteIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
+}
